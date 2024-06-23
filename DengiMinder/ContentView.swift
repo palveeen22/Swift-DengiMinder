cae632fc56bd8f4c195573f168f273240fb3128c
Alvin Pratama <palveeen22@gmail.com>
2024-06-23 21:14:24 +0700

    feat: initial commit

---
+++ DengiMinder/ContentView.swift
@@ -0,0 +1,19 @@
+//  ContentView.swift
+//  DengiMinder
+//  Expense Tracker App
+//  Created by Alvin Putra Pratama on 23/06/24.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .background(Color.black)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DengiMinder/DengiMinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DengiMinderApp.swift
+//  DengiMinder
+//
+//  Created by Alvin Putra Pratama on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct DengiMinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
